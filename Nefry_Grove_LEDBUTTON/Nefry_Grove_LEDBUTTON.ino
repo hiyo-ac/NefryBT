24addc5678aa5a7591e4c1fb128f0d74f9f93fad
ひよ/hiyo <32918187+hiyo-ac@users.noreply.github.com>
2018-05-07 21:09:52 +0900

    First Commit
    BUTTONモジュールを押下した時に、LEDモジュールのLEDを光らせるサンプルのコードです。

---
+++ Nefry_Grove_LEDBUTTON/Nefry_Grove_LEDBUTTON.ino
@@ -0,0 +1,27 @@
+#define LED A0      //LEDモジュールを指したGroveコネクタ
+#define BUTTON D2　 //BUTTONモジュールを指したGroveコネクタ
+
+void setup() {
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT);
+  Serial.print("LED = ");
+  Serial.print(LED);
+  Serial.print(", BUTTON = ");
+  Serial.println(BUTTON);
+}
+
+void loop() {
+  if (digitalRead(BUTTON)==1){ //BUTTONが押下された時の処理
+    digitalWrite(LED, HIGH); //LEDを発行させるコード
+    Serial.print("LED = ");
+    Serial.print(HIGH);
+  }else{
+    digitalWrite(LED, LOW); //LEDを停止するコード
+    Serial.print("LED = ");
+    Serial.print(LOW);
+  }
+  
+  Serial.print(", BUTTON = ");
+  Serial.println(digitalRead(BUTTON));
+  delay(300);
+}
